c3403c43be94a5b6e2063492d5139873a41323d8
mwettemwette <91114980+mwettemwette@users.noreply.github.com>
2023-03-30 10:26:47 +0200

    Add files via upload

---
+++ Scripts/Python/test.py
@@ -0,0 +1,16 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import pandas as pd
+
+# Connect to Google Sheets
+scope = ['https://www.googleapis.com/auth/spreadsheets',
+         "https://www.googleapis.com/auth/drive"]
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name("projet.json", scope)
+client = gspread.authorize(credentials)
+
+sheet = client.open("BDD_nom_id_projet").sheet1
+
+sheet.update('B8',"Emma")
+lst = sheet.get_all_values()
+print(lst)
