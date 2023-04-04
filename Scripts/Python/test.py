d744065974da6e6638e0966cda2e6067bb62a4d1
mwettemwette <91114980+mwettemwette@users.noreply.github.com>
2023-04-04 14:21:08 +0200

    dl clé api

--- Scripts/Python/test.py
+++ Scripts/Python/test.py
@@ -1,16 +1,21 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
+import gdown
 
 # Connect to Google Sheets
 scope = ['https://www.googleapis.com/auth/spreadsheets',
          "https://www.googleapis.com/auth/drive"]
 
-credentials = ServiceAccountCredentials.from_json_keyfile_name("Scripts\API_key\projet.json", scope)
-client = gspread.authorize(credentials)
+#credentials = ServiceAccountCredentials.from_json_keyfile_name("Scripts\API_key\projet.json", scope)
+#client = gspread.authorize(credentials)
 
-sheet = client.open("BDD_nom_id_projet").sheet1
+#sheet = client.open("BDD_nom_id_projet").sheet1
 
-sheet.update('B8',"Emma")
-lst = sheet.get_all_values()
-print(lst)
+#sheet.update('B8',"Emma")
+#lst = sheet.get_all_values()
+#print(lst)
+f = open('Scripts\Python\drive_api.txt','r')
+link = f.read()
+print(link)
+gdown.download(link,'Scripts\Python\projet.json')
