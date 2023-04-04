7779836a94f02d563ed2e18e3bd51cc99f4896f1
raspberry <rasp@berry.com>
2023-04-04 16:46:26 +0100

    essais avec haut-parleurs

--- Scripts/HP/HP.py
+++ Scripts/HP/HP.py
@@ -2,6 +2,6 @@ from scipy.io import wavfile as wav
 from scipy.fftpack import fft
 import numpy as np
 
-rate, data = wav.read('test.wav')
+rate, data = wav.read("test.wav")
 fft_out = fft(data)
-print(fft_out)
+
