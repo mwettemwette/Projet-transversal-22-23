65d19abab43443d034d3101a168fea9a0467cb75
Mwe <michelcourbin@gmail.com>
2023-04-18 14:41:43 +0200

    changement emplacement et code

--- Scripts/HP/HP.py
+++ Scripts/HP/HP.py
@@ -2,6 +2,6 @@ from scipy.io import wavfile as wav
 from scipy.fftpack import fft
 import numpy as np
 
-rate, data = wav.read("test.wav")
+rate, data = wav.read("Scripts\HP\test.wav")
 fft_out = fft(data)
 
