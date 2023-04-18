b9eaf5be272afa4533018ec80c243394f1bbd8cd
Mwe <michelcourbin@gmail.com>
2023-04-18 15:28:48 +0200

    maj mixer

--- Scripts/HP/HP.py
+++ Scripts/HP/HP.py
@@ -6,16 +6,16 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(26, GPIO.OUT)
 
 # Load the WAV file
-pygame.mixer.init()
-pygame.mixer.music.load("Scripts\HP\test.wav")
+mixer.init()
+mixer.music.load("Scripts\HP\test.wav")
 
 # Play the WAV file through the GPIO pin
 GPIO.output(26, GPIO.HIGH)
-pygame.mixer.music.play()
-while pygame.mixer.music.get_busy():
+mixer.music.play()
+while mixer.music.get_busy():
     continue
 GPIO.output(26, GPIO.LOW)
 
 # Clean up the GPIO pin and pygame
 GPIO.cleanup()
-pygame.mixer.quit()
+mixer.quit()
