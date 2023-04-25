19f35bcade1fc8907779f73f348d5b06ef944f62
Gaelle Leroux <gaelleleroux974@gmail.com>
2023-04-25 15:00:44 +0200

    amenagement code lecture bdd

--- Scripts/Python/BBD.py
+++ Scripts/Python/BBD.py
@@ -7,21 +7,24 @@ import gdown
 def trouver_nom(nom):
     try :
         cell = sheet.find(nom)
-        print(cell.row,cell.col)
+        return cell.row,cell.col
     except :
         print("Le nom n'est pas dans la base de donnee")
+        return None,None
 
 def trouver_nmb(nmb):
     try :
-        cell = sheet.find(nmb)
-        print(cell.row,cell.col)
-        return cell
+        cell = sheet.find(str(nmb))
+        return cell.row,cell.col
     except :
-        print("Le nom n'est pas dans la base de donnee")
-        return None
+        print("Le nombre n'est pas dans la base de donnee")
+        return None,None
 
-def get_x(row,col):
-    return 1
+def get_x(row,col,nmb):
+    return sheet.cell(row, col+nmb+1).value
+
+def get_y(row,col,nmb):
+    return sheet.cell(row, col+nmb+2).value
 
 
 # Connect to Google Sheets
@@ -30,19 +33,22 @@ scope = ['https://www.googleapis.com/auth/spreadsheets',
 
 credentials = ServiceAccountCredentials.from_json_keyfile_name("Scripts\Python\projet.json", scope)
 client = gspread.authorize(credentials)
-
 sheet = client.open("BDD_nom_id_projet").sheet1
 
-#sheet.update('B8',"Emma")
-lst = sheet.get_all_values()
-trouver_nom("a")
-ligne = sheet.row_values(3)
-print(ligne)
-print(lst)
-# f = open('Scripts\drive_api.txt','r')
-# link = f.read()
-# print(link)
-# gdown.download(link,'Scripts\Python\projet.json')
+
+# Instruction test
+x,y = trouver_nom("Julien")
+if (x!=None and y!=None):
+    print(get_x(x,y,0))
+    print(get_y(x,y,0))
+
+x,y = trouver_nmb(2)
+if (x!=None and y!=None):
+    print(get_x(x,y,1))
+    print(get_y(x,y,1))
+
+
+
 
 
 
