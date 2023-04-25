01acb0223d097311ea7384abdcad18a1ac2b88fb
Gaelle Leroux <gaelleleroux974@gmail.com>
2023-04-25 15:15:56 +0200

    try

--- Scripts/Python/BBD.py
+++ Scripts/Python/BBD.py
@@ -4,7 +4,7 @@ import pandas as pd
 import gdown
 
 
-def trouver_nom(nom):
+def trouver_nom(nom,sheet):
     try :
         cell = sheet.find(nom)
         return cell.row,cell.col
@@ -12,7 +12,7 @@ def trouver_nom(nom):
         print("Le nom n'est pas dans la base de donnee")
         return None,None
 
-def trouver_nmb(nmb):
+def trouver_nmb(nmb,sheet):
     try :
         cell = sheet.find(str(nmb))
         return cell.row,cell.col
@@ -20,32 +20,36 @@ def trouver_nmb(nmb):
         print("Le nombre n'est pas dans la base de donnee")
         return None,None
 
-def get_x(row,col,nmb):
+def get_x(row,col,nmb,sheet):
     return sheet.cell(row, col+nmb+1).value
 
-def get_y(row,col,nmb):
+def get_y(row,col,nmb,sheet):
     return sheet.cell(row, col+nmb+2).value
 
 
 # Connect to Google Sheets
-scope = ['https://www.googleapis.com/auth/spreadsheets',
-         "https://www.googleapis.com/auth/drive"]
+def connect_bdd():
+    scope = ['https://www.googleapis.com/auth/spreadsheets',
+            "https://www.googleapis.com/auth/drive"]
 
-credentials = ServiceAccountCredentials.from_json_keyfile_name("Scripts\Python\projet.json", scope)
-client = gspread.authorize(credentials)
-sheet = client.open("BDD_nom_id_projet").sheet1
+    credentials = ServiceAccountCredentials.from_json_keyfile_name("Scripts\Python\projet.json", scope)
+    client = gspread.authorize(credentials)
+    sheet = client.open("BDD_nom_id_projet").sheet1
+    return sheet
+
 
 
 # Instruction test
-x,y = trouver_nom("Julien")
+sheet = connect_bdd()
+x,y = trouver_nom("Julien",sheet)
 if (x!=None and y!=None):
-    print(get_x(x,y,0))
-    print(get_y(x,y,0))
+    print(get_x(x,y,0,sheet))
+    print(get_y(x,y,0,sheet))
 
-x,y = trouver_nmb(2)
+x,y = trouver_nmb(2,sheet)
 if (x!=None and y!=None):
-    print(get_x(x,y,1))
-    print(get_y(x,y,1))
+    print(get_x(x,y,1,sheet))
+    print(get_y(x,y,1,sheet))
 
 
 
